c375837d60802d741901e3857f6748acca397dfc
Maheeshi <132429336+Maheeshi@users.noreply.github.com>
2023-07-11 12:03:09 +0530

    Add files via upload

---
+++ Lab(07.11.23)/Q1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=?, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $x=6;
+    $y=4;
+    $sum=$x+$y;
+    $sub=$x-$y;
+    $mul=$x*$y;
+    $div=$x/$y;
+    echo "<br> Sum: $sum";
+    echo "<br> Differnce: $sub";
+    echo "<br> Product: $mul";
+    echo "<br> Division: $div";
+ ?>
+</body>
+</html>
---
+++ Lab(07.11.23)/Q2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=?, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    for($i=5; $i<=15; $i++);
+    echo $i." ";
+ ?>
+</body>
+</html>
